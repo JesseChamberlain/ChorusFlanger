3023b42fdd06de7e3e9bc5e2a0f0c6611740f57f
Jesse Chamberlain <jessechamberlain@JDC-Mac-Pro.local>
2022-09-08 20:54:55 -0400

    Rebuild UI

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -16,6 +16,50 @@ ChorusFlangerAudioProcessorEditor::ChorusFlangerAudioProcessorEditor (ChorusFlan
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+    
+    auto& params = processor.getParameters();
+    
+    // Set DryWet parameter
+    juce::AudioParameterFloat* dryWetParameter = (juce::AudioParameterFloat*)params.getUnchecked(0);
+    
+    mDryWetSlider.setBounds(0, 0, 100, 100);
+    mDryWetSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    mDryWetSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    mDryWetSlider.setRange(dryWetParameter->range.start, dryWetParameter->range.end);
+    mDryWetSlider.setValue(*dryWetParameter);
+    addAndMakeVisible(mDryWetSlider);
+    
+    mDryWetSlider.onValueChange = [this, dryWetParameter] { *dryWetParameter = mDryWetSlider.getValue();};
+    mDryWetSlider.onDragStart = [dryWetParameter] {dryWetParameter->beginChangeGesture();};
+    mDryWetSlider.onDragEnd = [dryWetParameter] {dryWetParameter->endChangeGesture();};
+    
+    // Set Feedback parameter
+    juce::AudioParameterFloat* feedbackParameter = (juce::AudioParameterFloat*)params.getUnchecked(1);
+    
+    mFeedbackSlider.setBounds(100, 0, 100, 100);
+    mFeedbackSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    mFeedbackSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    mFeedbackSlider.setRange(feedbackParameter->range.start, feedbackParameter->range.end);
+    mFeedbackSlider.setValue(*feedbackParameter);
+    addAndMakeVisible(mFeedbackSlider);
+    
+    mFeedbackSlider.onValueChange = [this, feedbackParameter] { *feedbackParameter = mFeedbackSlider.getValue();};
+    mFeedbackSlider.onDragStart = [feedbackParameter] {feedbackParameter->beginChangeGesture();};
+    mFeedbackSlider.onDragEnd = [feedbackParameter] {feedbackParameter->endChangeGesture();};
+    
+    // Set Feedback parameter
+    juce::AudioParameterFloat* delayTimeParameter = (juce::AudioParameterFloat*)params.getUnchecked(2);
+    
+    mDelayTimeSlider.setBounds(200, 0, 100, 100);
+    mDelayTimeSlider.setSliderStyle(juce::Slider::SliderStyle::RotaryVerticalDrag);
+    mDelayTimeSlider.setTextBoxStyle(juce::Slider::TextEntryBoxPosition::NoTextBox, true, 0, 0);
+    mDelayTimeSlider.setRange(delayTimeParameter->range.start, delayTimeParameter->range.end);
+    mDelayTimeSlider.setValue(*delayTimeParameter);
+    addAndMakeVisible(mDelayTimeSlider);
+    
+    mDelayTimeSlider.onValueChange = [this, delayTimeParameter] { *delayTimeParameter = mDelayTimeSlider.getValue();};
+    mDelayTimeSlider.onDragStart = [delayTimeParameter] {delayTimeParameter->beginChangeGesture();};
+    mDelayTimeSlider.onDragEnd = [delayTimeParameter] {delayTimeParameter->endChangeGesture();};
 }
 
 ChorusFlangerAudioProcessorEditor::~ChorusFlangerAudioProcessorEditor()
@@ -28,9 +72,9 @@ void ChorusFlangerAudioProcessorEditor::paint (juce::Graphics& g)
     // (Our component is opaque, so we must completely fill the background with a solid colour)
     g.fillAll (getLookAndFeel().findColour (juce::ResizableWindow::backgroundColourId));
 
-    g.setColour (juce::Colours::white);
-    g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+//    g.setColour (juce::Colours::white);
+//    g.setFont (15.0f);
+//    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
 }
 
 void ChorusFlangerAudioProcessorEditor::resized()
